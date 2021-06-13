b753327af37df140f1f5afad043cc585b28abb23
Kowalczyk <piotr.1.kowalczyk@nokia.com>
2021-06-13 16:30:13 +0200

    now can handle all one digit roman numbers

--- RomanNumerals.py
+++ RomanNumerals.py
@@ -1,7 +1,15 @@
+roman_to_arabic = {
+    'I': 1,
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+
 def from_roman(number):
-    if number == 'V':
-        return 5
-    if number == 'X':
-        return 10
-    return 1
+    return roman_to_arabic[number]
 
--- test_RomanNumerals.py
+++ test_RomanNumerals.py
@@ -6,7 +6,11 @@ import RomanNumerals
 @pytest.mark.parametrize("test_value, expected_value", [
     ('I', 1),
     ('V', 5),
-    ('X', 10)
+    ('X', 10),
+    ('L', 50),
+    ('C', 100),
+    ('D', 500),
+    ('M', 1000)
 ])
 def test_from_roman(test_value, expected_value):
     assert RomanNumerals.from_roman(test_value) == expected_value
